03efdcebcc9e3f8a9f1b22f0df2d17569500a7dd
emarkovic <enamarkovic27@gmail.com>
2017-05-09 11:51:55 -0700

    categories showing first question

--- iQuiz/iQuiz/QuestionViewController.swift
+++ iQuiz/iQuiz/QuestionViewController.swift
@@ -11,29 +11,61 @@ import UIKit
 class QuestionViewController: UIViewController {
     var questions: [Question]?
     var currentQuest = 0;
-    var previousButton: UIButton?
+    var previousBtnPressed: UIButton?
     
     @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var b0: UIButton!
+    @IBOutlet weak var b1: UIButton!
+    @IBOutlet weak var b2: UIButton!
+    @IBOutlet weak var b3: UIButton!
     
     override func viewDidLoad() {
+        let curQuestion = questions![currentQuest]
         super.viewDidLoad()
-        questionLabel.text = questions![currentQuest].question
-        // Do any additional setup after loading the view.
+        questionLabel.text = curQuestion.question
+        print(curQuestion.answers)
+        
+        b0.setTitle(curQuestion.answers[0], for: .normal)
+        b1.setTitle(curQuestion.answers[1], for: .normal)
+        b2.setTitle(curQuestion.answers[2], for: .normal)
+        b3.setTitle(curQuestion.answers[3], for: .normal)
     }
 
+    @IBAction func b0Pressed(_ sender: Any) {
+        clearPreviousPressed()
+        
+        b0.layer.borderWidth = 1
+        previousBtnPressed = b0
+    }
     
+    @IBAction func b1Pressed(_ sender: Any) {
+        clearPreviousPressed()
+        
+        b1.layer.borderWidth = 1
+        previousBtnPressed = b1
+    }
     
+    @IBAction func b2Pressed(_ sender: Any) {
+        clearPreviousPressed()
+        
+        b2.layer.borderWidth = 1
+        previousBtnPressed = b2
+    }
     
+    @IBAction func b3Pressed(_ sender: Any) {
+        clearPreviousPressed()
+        
+        b3.layer.borderWidth = 1
+        previousBtnPressed = b3
+    }
     
+    func clearPreviousPressed() {
+        if previousBtnPressed != nil {
+            previousBtnPressed!.layer.borderWidth = 0;
+        }
+    }
     
-    
-    
-    
-    
-    
-    
-    
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
